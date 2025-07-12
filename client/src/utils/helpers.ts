a10ba42f218253ed22fec84f1aebe55114ca3e1b
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-07-12 17:58:45 +0200

    helpers: added update

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -84,3 +84,17 @@ export function mergeByKey<T extends object, K extends keyof T>(
 
     return [...temp.values()];
 }
+
+export function update<T extends object>(
+    arr: T[],
+    predicate: (item: T, index: number, array: T[]) => boolean,
+    payload: Partial<T>
+) {
+    const targetIndex = arr.findIndex(predicate);
+
+    if (targetIndex > -1) {
+        return arr.with(targetIndex, { ...arr[targetIndex], ...payload } as T);
+    }
+
+    return arr;
+}
