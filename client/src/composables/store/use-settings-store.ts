8d8f2b63ffb406f853532cfdca2534661b756e7f
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-07-13 19:33:50 +0200

    composables: added setting store

---
+++ client/src/composables/store/use-settings-store.ts
@@ -0,0 +1,24 @@
+import { reactive, toRefs, watchEffect } from 'vue';
+import { getStorageItem, setStorageItem } from '../../utils/storage';
+
+interface State {
+    audioDeviceId: string;
+    videoDeviceId: string;
+}
+
+const state = reactive<State>({
+    audioDeviceId: getStorageItem('audioDeviceId') || null,
+    videoDeviceId: getStorageItem('videoDeviceId') || null
+});
+
+export function useSettingsStore() {
+    watchEffect(() => {
+        setStorageItem('videoDeviceId', state.videoDeviceId);
+
+        setStorageItem('audioDeviceId', state.audioDeviceId);
+    });
+
+    return {
+        ...toRefs(state)
+    };
+}
