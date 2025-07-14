8e3f49919348dded9571ea5d5e130f77405fa62d
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-07-14 12:54:41 +0200

    Contain: using useResizeObserver

--- client/src/components/base/Contain.vue
+++ client/src/components/base/Contain.vue
@@ -1,46 +1,28 @@
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted, ref, watch } from 'vue';
+import { useResizeObserver } from '@vueuse/core';
+import { ref, useTemplateRef, watch } from 'vue';
 
 const props = defineProps<{ aspectRatio: number }>();
 
-const container = ref<HTMLDivElement | null>(null);
-const resizeObserver = ref<ResizeObserver | null>(null);
+const container = useTemplateRef('container');
 const containerClass = ref<'w-full' | 'h-full'>('w-full');
 
 function calculateLayout() {
-    const { aspectRatio } = props;
+    requestAnimationFrame(() => {
+        const { aspectRatio } = props;
 
-    if (container.value && aspectRatio) {
-        const hScale = container.value.offsetWidth / aspectRatio;
-        const vScale = container.value.offsetHeight;
+        if (container.value && aspectRatio) {
+            const hScale = container.value.offsetWidth / aspectRatio;
+            const vScale = container.value.offsetHeight;
 
-        containerClass.value = hScale <= vScale ? 'w-full' : 'h-full';
-    }
-}
-
-function enableObserver() {
-    if (container.value) {
-        resizeObserver.value = new ResizeObserver(() =>
-            requestAnimationFrame(calculateLayout)
-        );
-
-        resizeObserver.value.observe(container.value);
-    }
-}
-
-function disableObserver() {
-    if (resizeObserver.value) {
-        resizeObserver.value.disconnect();
-
-        resizeObserver.value = null;
-    }
+            containerClass.value = hScale <= vScale ? 'w-full' : 'h-full';
+        }
+    });
 }
 
 watch(() => props.aspectRatio, calculateLayout);
 
-onMounted(enableObserver);
-
-onBeforeUnmount(disableObserver);
+useResizeObserver(container, calculateLayout);
 </script>
 
 <template>
