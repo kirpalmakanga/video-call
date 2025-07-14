5f359f6dbc65d0f30851b2d64e60c3bc1184dc96
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-07-14 17:48:55 +0200

    installed NuxtUI

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,12 @@
 import { createApp } from 'vue';
+import ui from '@nuxt/ui/vue-plugin';
 import App from './App.vue';
 import router from './router';
 import './assets/styles/main.css';
 import './assets/styles/main.scss';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+app.use(ui);
+app.mount('#app');
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import tailwindcss from '@tailwindcss/vite';
+import ui from '@nuxt/ui/vite';
 
 export default defineConfig({
-    plugins: [tailwindcss(), vue()],
+    plugins: [vue(), ui()],
     server: { allowedHosts: true }
 });
