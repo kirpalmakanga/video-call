2650c59db47b3becb788db7c5a0b26b858effda4
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-07-15 16:31:23 +0200

    useSettingsStore: added isAudioEnabled and isVideoEnabled to state

--- client/src/composables/store/use-settings-store.ts
+++ client/src/composables/store/use-settings-store.ts
@@ -3,14 +3,18 @@ import { getStorageItem, setStorageItem } from '../../utils/storage';
 
 interface State {
     displayName: string;
+    isAudioEnabled: boolean;
     audioDeviceId: string;
+    isVideoEnabled: string;
     videoDeviceId: string;
 }
 
 const state = reactive<State>(
     getStorageItem('settings') || {
         displayName: '',
+        isAudioEnabled: true,
         audioDeviceId: '',
+        isVideoEnabled: true,
         videoDeviceId: ''
     }
 );
