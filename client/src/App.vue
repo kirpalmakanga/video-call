74153219bec761bacc9eb66cf6726cdeb6cc4e1b
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-07-20 13:01:47 +0200

    Added offline indicator

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
+import { useOnline } from '@vueuse/core';
 import { RouterView } from 'vue-router';
+
+const isOnline = useOnline();
 </script>
 
 <template>
+    <div
+        v-if="!isOnline"
+        class="flex items-center bg-warning-900 text-neutral-100 text-xs rounded mt-4 mx-4 p-2"
+    >
+        <UIcon class="size-5 mr-2" name="i-mdi-wifi-strength-off-outline" />
+        Offline: please check your internet connection.
+    </div>
+
     <UApp><RouterView /></UApp>
 </template>
