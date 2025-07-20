686987039c5943425bdaf0ed46f4fb70f9e20280
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-07-21 00:19:59 +0200

    utils: removed unused functions

--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -4,56 +4,6 @@ interface ThrottleOptions {
     once?: boolean;
 }
 
-interface Throttler<T extends unknown[]> {
-    (...args: T): void;
-    cancel(): void;
-}
-
-export const throttle = <T extends unknown[]>(
-    callback: (...args: T) => unknown,
-    wait = 0,
-    { start = true, middle = true, once = false }: ThrottleOptions = {}
-): Throttler<T> => {
-    let last = 0;
-    let timer: ReturnType<typeof setTimeout>;
-    let cancelled = false;
-
-    function fn(this: unknown, ...args: T) {
-        if (cancelled) return;
-
-        const delta = Date.now() - last;
-
-        last = Date.now();
-
-        if (start) {
-            start = false;
-
-            callback.apply(this, args);
-
-            if (once) fn.cancel();
-        } else if ((middle && delta < wait) || !middle) {
-            clearTimeout(timer);
-
-            timer = setTimeout(
-                () => {
-                    last = Date.now();
-                    callback.apply(this, args);
-                    if (once) fn.cancel();
-                },
-                !middle ? wait : wait - delta
-            );
-        }
-    }
-
-    fn.cancel = () => {
-        clearTimeout(timer);
-
-        cancelled = true;
-    };
-
-    return fn;
-};
-
 export function omit<T extends object, K extends keyof T>(
     base: T,
     ...keys: K[]
@@ -108,17 +58,4 @@ export function update<T extends object>(
     }
 
     return arr;
-}
-
-export function debounce<A extends unknown[]>(
-    callback: (...args: A) => void,
-    delay: number
-) {
-    let timer: ReturnType<typeof setTimeout>;
-
-    return (...args: A) => {
-        if (timer) clearTimeout(timer);
-
-        timer = setTimeout(() => callback(...args), delay);
-    };
 }
