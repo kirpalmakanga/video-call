f5c0bfb27371daf67d38bd6391533ff02f46940b
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-08-08 13:31:21 +0200

    Home: added filter input

--- client/src/components/RoomsGrid.vue
+++ client/src/components/RoomsGrid.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import RoomsGridItem from './RoomsGridItem.vue';
 
-defineProps<{ rooms: Room[] }>();
+defineProps<{ items: Room[] }>();
 </script>
 
 <template>
     <ul
         class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"
     >
-        <li v-for="{ id, name } of rooms">
+        <li v-for="{ id, name } of items">
             <RoomsGridItem :id="id" :name="name" />
         </li>
     </ul>
--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,20 +1,45 @@
 <script setup lang="ts">
+import { computed, ref } from 'vue';
 import Placeholder from '../components/base/Placeholder.vue';
 import RoomsGrid from '../components/RoomsGrid.vue';
 import RoomsGridSkeleton from '../components/RoomsGridSkeleton.vue';
 import { useRoomsListQuery } from '../utils/queries';
 
 const { data: rooms, isLoading } = useRoomsListQuery();
+
+const filter = ref<string>('');
+
+const currentRooms = computed(() => {
+    if (!rooms.value) {
+        return [];
+    }
+
+    if (filter.value) {
+        return rooms.value.filter(({ name }) =>
+            name.toLowerCase().includes(filter.value.toLowerCase())
+        );
+    }
+
+    return rooms.value;
+});
 </script>
 
 <template>
     <div class="flex grow md:justify-center">
         <div class="md:rounded p-4 w-full">
-            <p class="text-neutral-100 mb-4">Available rooms</p>
+            <h1 class="text-neutral-100 mb-4">Available rooms</h1>
 
             <RoomsGridSkeleton v-if="isLoading" />
 
-            <RoomsGrid v-else-if="rooms?.length" :rooms="rooms" />
+            <template v-else-if="rooms?.length">
+                <SearchForm
+                    class="bg-gray-900 p-4 rounded mb-4"
+                    label="Find a room"
+                    v-model="filter"
+                />
+
+                <RoomsGrid :items="currentRooms" />
+            </template>
 
             <Placeholder
                 v-else
