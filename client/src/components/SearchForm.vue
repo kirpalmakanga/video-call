3b578df0077a13dac03ecd5cc0b303d0b8003cb4
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-08-08 13:28:37 +0200

    new component: SearchForm

---
+++ client/src/components/SearchForm.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps<{ label?: string }>();
+
+const model = defineModel<string>();
+</script>
+
+<template>
+    <UFormField :label="label">
+        <UInput
+            class="w-full"
+            variant="soft"
+            placeholder="Room name"
+            v-model="model"
+        />
+    </UFormField>
+</template>
