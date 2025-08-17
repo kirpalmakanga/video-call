8bd25134d855ebf6b0bf7bfc24707c5db3ec1980
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-08-17 19:02:53 +0200

    cors.middleware: refactoring

--- server/src/middlewares/cors.middleware.ts
+++ server/src/middlewares/cors.middleware.ts
@@ -1,16 +1,18 @@
-import { handleCors, type H3Event } from 'h3';
+import { handleCors, type CorsOptions, type H3Event } from 'h3';
 
 const { CLIENT_URI } = process.env;
 
+const corsOptions: CorsOptions = {
+    origin: [CLIENT_URI as string],
+    preflight: {
+        statusCode: 204
+    },
+    methods: '*'
+};
+
 export function useCors() {
     return (event: H3Event) => {
-        const corsResponse = handleCors(event, {
-            origin: [CLIENT_URI as string],
-            preflight: {
-                statusCode: 204
-            },
-            methods: '*'
-        });
+        const corsResponse = handleCors(event, corsOptions);
 
         if (typeof corsResponse === 'string') {
             return corsResponse;
