1a96d523cb4ca233de88011c19b56c40bed70057
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-08-17 12:57:20 +0200

    replaced bcrypt by bcryptjs

--- server/src/utils/auth.utils.ts
+++ server/src/utils/auth.utils.ts
@@ -1,4 +1,4 @@
-import { compare, hash } from 'bcrypt';
+import { compare, hash } from 'bcryptjs';
 import { createHash, randomBytes } from 'crypto';
 
 export function validatePassword(
@@ -8,12 +8,12 @@ export function validatePassword(
     return compare(targetPassword, storedPassword);
 }
 
-export function hashToken(token: string) {
-    return createHash('sha256').update(token).digest('hex');
-}
-
 export function hashPassword(password: string) {
     return hash(password, 12);
+}
+
+export function hashToken(token: string) {
+    return createHash('sha256').update(token).digest('hex');
 }
 
 export function createVerificationToken() {
