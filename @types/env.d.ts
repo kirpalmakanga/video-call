7f8704a085cfb251303c9c78a254413ea7918dc5
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-08-19 01:05:35 +0200

    updated client types

--- @types/env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="vite/client" />
-interface ImportMeta {
-    readonly env: Record<string, string>;
-}
---
+++ client/@types/env.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="vite/client" />
+interface ImportMeta {
+    readonly env: {
+        VITE_API_URI: string;
+        VITE_SOCKET_URI: string;
+    };
+}
