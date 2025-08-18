37e1e7e394323094027460da356a45b6a44d6ec6
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-08-18 23:51:07 +0200

    added env variables to global server types

--- server/@types/global.d.ts
+++ server/@types/global.d.ts
@@ -2,4 +2,28 @@
 
 export {};
 
-declare global {}
+declare global {
+    namespace NodeJS {
+        interface ProcessEnv {
+            // Base
+            NODE_ENV: 'development' | 'production';
+            PORT: string;
+            CLIENT_URI: string;
+            API_URI: string;
+            DATABASE_URL: string;
+
+            // JWT
+            JWT_ACCESS_SECRET: string;
+            JWT_ISSUER: string;
+            JWT_AUDIENCE: string;
+
+            //Email
+            SMTP_HOST: string;
+            SMTP_PORT: string;
+            SMTP_SECURE: 'true' | 'false';
+            SMTP_USER: string;
+            SMTP_PASS: string;
+            MAIL_FROM: string;
+        }
+    }
+}
