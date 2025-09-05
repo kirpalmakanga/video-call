196f088556c4a3872284c9b7d2d18467c3dd32d8
Kirpal Makanga <makanga.kirpal@gmail.com>
2025-09-05 21:17:43 +0200

    client/auth: added back links to reset and forgot password pages

--- client/src/pages/ResetPassword.vue
+++ client/src/pages/ResetPassword.vue
@@ -5,6 +5,13 @@ import ResetPasswordForm from '../components/auth/ResetPasswordForm.vue';
 <template>
     <div class="flex grow items-center justify-center p-4">
         <div class="bg-gray-900 rounded p-4 md:w-sm shadow">
+            <ULink class="inline-flex items-center text-sm gap-1" href="/login">
+                <UIcon class="size-4" name="i-mdi-arrow-left" />
+                Back to sign in form
+            </ULink>
+
+            <USeparator class="my-2" />
+
             <h1 class="mb-6 font-bold text-center">Reset password</h1>
 
             <ResetPasswordForm />
