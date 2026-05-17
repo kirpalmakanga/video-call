125a42c7cab0154e9b46981b3df7276900e2ca15
kirpalmakanga <makanga.kirpal@gmail.com>
2026-05-17 02:31:43 +0200

    useParticipantsList: refactoring

--- client/src/composables/use-participants-list.ts
+++ client/src/composables/use-participants-list.ts
@@ -4,24 +4,30 @@ import { update } from '../utils/helpers';
 export function useParticipantsList() {
     const participants = ref<ClientParticipant[]>([]);
 
+    function matchParticipant(targetParticipantId: string) {
+        return ({ id }: ClientParticipant) => id === targetParticipantId;
+    }
+
     return {
         participants,
         setParticipant: (participant: ClientParticipant) => {
-            if (participants.value.some(({ id }) => id === participant.id)) {
+            if (participants.value.some(matchParticipant(participant.id))) {
                 participants.value = update(
                     participants.value,
-                    ({ id }) => id === participant.id,
+                    matchParticipant(participant.id),
                     participant
                 );
             } else {
                 participants.value.push(participant);
             }
         },
-        removeParticipant: (participantId: string) => {
-            participants.value = participants.value.filter(({ id }) => id !== participantId);
+        removeParticipant: (targetParticipantId: string) => {
+            participants.value = participants.value.filter(
+                (participant) => !matchParticipant(targetParticipantId)(participant)
+            );
         },
-        toggleMuteParticipant: (participantId: string) => {
-            const participant = participants.value.find(({ id }) => id === participantId);
+        toggleMuteParticipant: (targetParticipantId: string) => {
+            const participant = participants.value.find(matchParticipant(targetParticipantId));
 
             if (participant) {
                 participant.isLocallyMuted = !participant.isLocallyMuted;
